42d81b63c9d08274c54f9c3b264d5e21832e3147
Julian <Lunnaris01@users.noreply.github.com>
2025-01-17 04:33:08 +0100

    chapter 2

--- app.py
+++ app.py
@@ -1,118 +1,68 @@
 import pandas as pd
-from flask import Flask, request, make_response, render_template, redirect, url_for, Response, send_from_directory,jsonify
-import datetime
+from flask import Flask, request, make_response, render_template, redirect, url_for, Response, send_from_directory, \
+    jsonify, session, flash
 import os
 import uuid
 import pandas
 
-app = Flask(__name__, template_folder='templates')
-
-
-@app.route('/', methods=['GET', 'POST'])
+app = Flask(__name__, template_folder='templates', static_folder='static', static_url_path="/")
+app.secret_key = 'SOME KEY'
+@app.route('/index')
 def index():
-    if request.method == 'GET':
-        return render_template('index.html', year=datetime.datetime.now().year, foottext="This is the foottext")
+    return render_template("index.html", message='')
 
-@app.route('/index_post',methods = ['GET', 'POST'])
-def index_post():
+@app.route('/set_data')
+def set_data():
+    session['name'] = 'Julian'
+    session['other'] = 'Hello World'
+    return render_template('index.html', message= 'Session Data has been set')
+
+@app.route('/get_data')
+def get_data():
+    if (session):
+        name = session.get('name')
+        other = session.get('other')
+        return render_template('index.html', message = f'Session Data: {name}, {other}')
+    return render_template('index.html', message = "Missing Session Data")
+
+@app.route('/clear_session')
+def clear_session():
+    session.clear()
+    return render_template('index.html', message = 'Session data has been cleared')
+
+@app.route('/set_cookie')
+def set_cookie():
+    response = make_response(render_template('index.html', message = 'Cookie has been set'))
+    response.set_cookie('cookie_name', 'cookie_value')
+    return response
+
+@app.route('/get_cookie')
+def get_cookie():
+    cookie_value = request.cookies.get('cookie_name')
+    return render_template('index.html', message = f'Cookie Value: {cookie_value}')
+
+@app.route('/remove_cookie')
+def remove_cookie():
+    response = make_response(render_template('index.html', message = 'Cookie has been removed'))
+    response.set_cookie('cookie_name', expires=0)
+    return response
+
+@app.route('/',methods=['POST','GET'])
+def login():
     if request.method == 'GET':
-        return render_template('index_post.html', year=datetime.datetime.now().year, foottext="Foottext for the Post Requests Page")
+        return render_template('login.html')
     elif request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-
-        if username == 'admin' and password == 'password':
-            return render_template('member_landing_page.html', username="admin")
+        username = request.form['username']
+        password = request.form['password']
+        if username == 'admin' and password == '12345':
+            flash('Login Successful')
+            return render_template('index.html',message = 'Login Successful')
         else:
-            return render_template('index_post_wrongpass.html', year=datetime.datetime.now().year,foottext="Wrong username or password")
-
-
-@app.route('/member_landing_page')
-def member_landing_page(username):
-    return render_template('member_landing_page.html', username="admin")
-
-@app.route('/index_2')
-def index2():
-    today = datetime.datetime.now()
-    return render_template('index_2.html', year=datetime.datetime.now().year, date=[today.day, today.month, today.year])
-
-
-@app.route('/redirect_endpoint')
-def redirect_endpoint():
-    return redirect(url_for('index'))
-
-@app.route('/file_upload',methods = ['GET', 'POST'])
-def file_upload():
-    file = request.files.get('file')
-
-    if file.content_type == 'text/plain':
-        return file.read().decode()
-    elif file.content_type == 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' or file.content_type == 'application/vnd.ms-excel':
-        df = pd.read_excel(file)
-        return df.to_html()
-    else:
-        return ""
-
-@app.route('/convert_csv',methods = ['GET', 'POST'])
-def convert_csv():
-    if request.method == 'POST':
-        file = request.files.get('file')
-
-        exel_file = pd.read_excel(file)
-        response = Response(
-            exel_file.to_csv(),
-            mimetype='text/csv',
-            headers ={
-                'Content-Disposition': 'attachment; filename=result.csv'
-            }
-        )
-        return response
-
-@app.route("/convert_csv_downloadpage",methods = ['GET', 'POST'])
-def convert_csv_downloadpage():
-    file = request.files.get('file')
-    exel_file = pd.read_excel(file)
-
-
-    os.makedirs("downloads",exist_ok=True)
-    filename = f'{uuid.uuid4()}.csv'
-
-    exel_file.to_csv(os.path.join('downloads',filename))
-    return render_template("memberpage_download.html", filename=filename)
-
-@app.route("/download/<filename>")
-def download(filename):
-    return send_from_directory("downloads", filename,download_name="result.csv")
-
-
-@app.route("/handle_post_json",methods = ['POST'])
-def handle_post_json():
-    greeting = request.json.get('greeting')
-    name = request.json.get('name')
-    with open('file.txt','w') as f:
-        f.write(f'{greeting}, {name}')
-
-    return jsonify({'message': 'Successfully written file.'})
-
-@app.route("/hello", methods=['GET', 'POST'])
-def hello():
-    if request.method == 'GET':
-        response = make_response("Hello to your GET request!\n")
-        response.status_code = 202
-        response.headers['Content-Type'] = 'text/plain'
-        return response
-    elif request.method == 'POST':
-        return "We say Hello to your POST request\n", 202
-
-
-@app.template_filter('reverse_string')
-def reverse_string(s):
-    return s[::-1]
-
-
-@app.template_filter('slice')
-def slice(s, start, end):
-    return s[start:end]
+            flash('Login Failed')
+            return render_template('index.html',message = 'Login Failed')
+@app.route('/funwithimages')
+def funwithimages():
+    return render_template("funwithimages.html")
 
 
 if __name__ == '__main__':
